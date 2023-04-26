50e232249b6a76df69d611e6034a2048ff44c4c8
JuneKei <wnsrjs1@gmail.com>
2023-04-26 09:30:28 +0900

    add Header

---
+++ front/src/components/Header.js
@@ -0,0 +1,15 @@
+import styled, { css } from "styled-components";
+
+function Header(){
+    return(
+        <TopNavigation>
+            헤더
+        </TopNavigation>
+    )
+}
+
+const TopNavigation = styled.div`
+    width: 100%;
+`;
+
+export default Header;
